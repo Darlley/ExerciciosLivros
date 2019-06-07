63fc2db1d5064d273d54583c44b8791a3d3d0f28
Darlley <37590954+Darlley@users.noreply.github.com>
2019-06-07 15:05:00 -0400

    Create ex01.js

---
+++ tecnologia/eloquentejavascript/ex01.js
@@ -0,0 +1,18 @@
+/*
+Escreva um programa que faça sete chamadas a console.log() para retornar o seguinte triângulo:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
+
+var tri = '';
+for(var i = 1; i <= 7; i++){
+	tri += '#';
+  console.log(tri);
+};
