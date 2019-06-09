33242b37e6bfdc0a3e2ca10faa0aa694cce7da7c
Darlley <37590954+Darlley@users.noreply.github.com>
2019-06-09 15:53:01 -0400

    Update ex03.js

--- tecnologia/eloquentejavascript/segundocapitulo/ex03.js
+++ tecnologia/eloquentejavascript/segundocapitulo/ex03.js
@@ -1,5 +1,6 @@
 /* PERGUNTA:
 
+Escreva um programa que cria uma string que representa uma grade 8x8, usando novas linhas para separar os caracteres. A cada posição da grade existe um espaço ou um caractere "#". Esses caracteres formam um tabuleiro de xadrez.
 */
 
 // RESPOSTA:
