e86d9303dac0dfa483ad5b5ffd6ee49a79445409
Darlley <37590954+Darlley@users.noreply.github.com>
2019-06-09 15:51:11 -0400

    Create ex03.js

---
+++ tecnologia/eloquentejavascript/segundocapitulo/ex03.js
@@ -0,0 +1,18 @@
+/* PERGUNTA:
+
+*/
+
+// RESPOSTA:
+
+var tab;
+for(var i=0; i<=7;i++){
+	for(var j=0; j<=7;i++){
+		if(i%2==0 && j){
+			tab = "#";
+			console.log(tab);
+        }else{
+			tab = " ";
+			console.log(tab);
+        }
+	}
+}
